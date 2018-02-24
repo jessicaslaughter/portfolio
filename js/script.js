1fb14711804c71654427174dedbbc16075f81978
Jessica <jessicaslaughter@utexas.edu>
2018-02-23 19:19:02 -0600

    updated projects and bio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function show(id) {
+	var project = document.getElementById(id);
+	var pos = window.scrollY;
+	pos = pos + (pos * .05);
+	project.style.top = pos + 'px';
+	project.style.zIndex = 100;
+	// project.style.display = 'block';
+	project.classList.remove("hide");
+	project.classList.add("show");
+}
+
+function hide(id) {
+	var project = document.getElementById(id);
+	// project.style.display = 'none';
+	project.classList.remove("show");
+	project.classList.add("hide");
+	setTimeout(function(){ project.style.zIndex = -100; }, 200);
+}
+
